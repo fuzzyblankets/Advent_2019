bebbb38a1b3af852c1e86e41ac35bee48abc78ee
Ellie <tutt1990@gmail.com>
2020-04-13 08:24:44 -0600

    Created class to add definitions to instructions Overall cleanup

--- Day5/Part1.py
+++ Day5/Part1.py
@@ -8,86 +8,101 @@ https://adventofcode.com/2019/day/2#part2
 #             1, 10, 103, 107, 1, 107, 9, 111, 2, 111, 10, 115, 1, 115, 9, 119, 1, 13, 119, 123, 1, 123,
 #             9, 127, 1, 5, 127, 131, 2, 13, 131, 135, 1, 9, 135, 139, 1, 2, 139, 143, 1, 13, 143, 0, 99, 2, 0, 14, 0]
 
-parameter_count_dict = {'01':3, '02':3, '03':1, '04':1, '99':0}
-
-def instruction_split(instruct):
-    opcode_and_modes = str(instruct[0]).zfill(5)
-    opcode = opcode_and_modes[-2:]
-    parameters = instruct[1:]
-    modes = list(reversed(list(opcode_and_modes[:len(parameters)])))
-    parameters = parameter_discovery(zip(parameters, modes))
-    return opcode, parameters
+from enum import Enum
 
 
-def parameter_discovery(inputs):
-    #TODO figure out how parameter mode applies to writing values out
+class OPERATION_CODE(Enum):
+    ADD = 1
+    MULTIPLY = 2
+    INPUT = 3
+    OUTPUT = 4
+    TERMINATE = 99
+
+
+class MODE(Enum):
+    POSITION = 0
+    IMMEDIATE = 1
+
+
+class Instruction:
+    parameter_count_dict = {'01': 3, '02': 3, '03': 1, '04': 1, '99': 0}
+    def __init__(self, opcode):
+        self.ops = OPERATION_CODE(int(opcode[-2:]))
+        mode_list = list(map(int, opcode[:3][::-1]))
+        self.mode = list(MODE(i) for i in mode_list)
+        self.parameter_count = self.parameter_count_dict[opcode[-2:]]
+
+    def __str__(self):
+        return "Opcode: {}\n Mode: {} \nParameters: {}".format(self.ops, self.mode, self.parameter_count)
+
+
+class OPERATION():
+    def add(param):
+        int_code[param[2]] = param[0] + param[1]
+
+    def multiply(param):
+        int_code[param[2]] = param[0] * param[1]
+
+    def put_in(param, value):
+        int_code[param[0]] = value
+
+    def put_out(param):
+        return int_code[param[0]]
+
+def parameter_discovery(params):
     parameters=[]
-    for i in inputs:
-        if i[1] == '0':  # positional
-            position  = i[0]
-            parameters.append(int_code[position])
-        elif i[1] == '1':  # absolute
-            absolute = i[0]
-            parameters.append(absolute)
+    for param in params:
+        if param[1] is MODE.POSITION:  # positional
+            parameters.append(int_code[param[0]])
+        elif param[1] is MODE.IMMEDIATE:  # absolute
+            parameters.append(param[0])
         else:
-            raise ValueError("Incorrect parameter mode: {}".format(i[1]))
+            raise ValueError("Incorrect parameter mode: {}".format(param[1]))
     return parameters
-
-
-def op_01(param1, param2, param3):
-    result = param1 + param2
-    int_code[param3] = result
-
-
-def op_02(param1, param2, param3):
-    result = int_code[param1] * int_code[param2]
-    int_code[param3] = result
-
-def op_03(param1, value):
-    int_code[param1] = value
-
-def op_04(param1):
-    print (int_code[param1])
 
 
 def main(int_code):
     instruction_pointer = 0
     while instruction_pointer < len(int_code):
-        opcode = str(int_code[instruction_pointer]).zfill(2)
-        if opcode in parameter_count_dict:
-            parameter_count = parameter_count_dict[opcode]
+        instruction = Instruction(str(int_code[instruction_pointer]).zfill(5))
+        parameters = int_code[instruction_pointer+1:instruction_pointer+instruction.parameter_count+1]
+        if len(parameters) >1:
+            parameters_values = parameter_discovery(zip(parameters[:-1], instruction.mode))
+            last_value = parameters[-1]
+            parameters_values.append(last_value)
         else:
-            raise ValueError("Opcode parameter count not defined in dictionary.")
-        instruction_length = parameter_count+1 #parameters plus opcode
-        opcode, parameters = instruction_split(int_code[instruction_pointer:instruction_pointer+instruction_length])
+            parameters_values = parameters
 
-        if opcode == '01':
-            op_01(parameters[0],parameters[1],parameters[2])
-        elif opcode == '02':
-            op_02(parameters[0],parameters[1],parameters[2])
-        elif opcode == '03':
+
+        if instruction.ops is OPERATION_CODE.ADD:
+            OPERATION.add(parameters_values)
+
+        elif instruction.ops is OPERATION_CODE.MULTIPLY:
+            OPERATION.multiply(parameters_values)
+
+        elif instruction.ops is OPERATION_CODE.INPUT:
             value = int(input("Please enter the ID of the system to test:\n"))
-            op_03(parameters[0], value)
-        elif opcode == '04':
-            op_04(parameters[0])
-        elif opcode == '99':
+            OPERATION.put_in(parameters_values, value)
+
+        elif instruction.ops is OPERATION_CODE.OUTPUT:
+            print(OPERATION.put_out(parameters_values))
+
+        elif instruction.ops is OPERATION_CODE.TERMINATE:
             print ("Identified End Marker. Halting Code.")
             break
+
         else:
             raise ValueError("Invalid opcode provided.\n Opcode: {}".format(opcode))
 
-        instruction_pointer += instruction_length
+        instruction_pointer += (instruction.parameter_count+1)
+
 
 if __name__ == '__main__':
     int_code =[]
     puzzle_input_path = "/Users/Ellie/PycharmProjects/Advent_2019/Day5/puzzle_input.txt"
     with open(puzzle_input_path, 'r') as file:
-        for line in file:
-            puzzle_input = line.strip().split(",")
-            for entry in puzzle_input:
-                int_code.append(int(entry))
-
-    print(int_code[225])
+        puzzle_input = file.read().strip().split(",")
+        int_code = list(map(int, puzzle_input))
 
     main(int_code)
 
