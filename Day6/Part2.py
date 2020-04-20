4b41a58757d7711cacd9fd0ab6d48791966c437c
Aaron <tutt1990@gmail.com>
2020-04-19 18:02:36 -0600

    solved day 6, parts 1 and 2.  Gave up on trying to nest dicts over and over.  instead used created a dictionary for each value, connected splits in an array.

--- Day6/Part2.py
+++ Day6/Part2.py
@@ -8,11 +8,10 @@ from collections import ChainMap
 def main(puzzle_input):
     orbit_map = {}
     orbit_map = puzzle_split(puzzle_input, orbit_map)
-    total_orbits = 0
-    for orbit_key in orbit_map.keys():
-        for orbit in orbit_map[orbit_key]:
-            total_orbits += orbit_count(orbit_map, orbit)
-    return total_orbits
+    orbit_change_count = orbit_change(orbit_map, "YOU", "SAN")
+    total_orbit_count = total_orbits(orbit_map)
+    return orbit_change_count
+
 
 def puzzle_split(puzzle_input, orbit_map):
     for orbits in puzzle_input:
@@ -24,6 +23,36 @@ def puzzle_split(puzzle_input, orbit_map):
         else:
             orbit_map[orbitee].append(orbiter)
     return orbit_map
+
+def total_orbits(orbit_map):
+    total_orbits = 0
+    for orbit_key in orbit_map.keys():
+        for orbit in orbit_map[orbit_key]:
+            total_orbits += orbit_count(orbit_map, orbit)
+    return total_orbits
+
+
+def orbit_change(dict, start, end):
+    start_path = orbit_path(dict, start)
+    end_path = orbit_path(dict, end)
+
+    for i in start_path:
+        if i in end_path:
+            start_match = start_path.index(i)
+            end_match = end_path.index(i)
+            return start_match+end_match
+
+
+
+def orbit_path(orbit_map, orbit, dig=True):
+    path_list = []
+    while dig:
+        dig, key = depth(orbit_map, orbit)
+        if dig:
+            orbit = key
+            path_list.append(key)
+    return path_list
+
 
 def depth (dict, value):
     for k, v in dict.items():
@@ -55,7 +84,7 @@ if __name__ == '__main__':
     with open(puzzle_input_path, 'r') as file:
         puzzle_input = file.read().strip().split("\n")
 
-    total_orbit_output = main(puzzle_input)
+    orbit_change_output = main(puzzle_input)
 
-    print("Total orbit count: {}".format(total_orbit_output))
+    print("Orbit change count: {}".format(orbit_change_output))
 
