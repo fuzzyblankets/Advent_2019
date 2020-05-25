58dbdc69f95dec9ede5a30356c52701410d05414
tutt1990 <tutt1990@gmail.com>
2020-05-25 08:32:16 -0600

    day 7 - separate files

--- Day7/Part 2/main.py
+++ Day7/Part 2/main.py
@@ -2,14 +2,30 @@
 https://adventofcode.com/2019/day/7
 """
 
+import argparse
+from payloads import PAYLOAD
+
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Int Code Options.')
+    parser.add_argument('-l', '--loopback-mode', action='store_true', type=str,
+                        dest='loopback_mode', help='turn on thruster loopback mode')
+    args = parser.parse_args()
+
+    loopback_mode = False
+    if args.loopback_mode:
+        loopback_mode = args.loopback_mode
+
     int_code =[]
-    puzzle_input_path = "/root/PycharmProjects/Advent_2019/Day7/puzzle_input_test.txt"
+    puzzle_input_path = "/root/PycharmProjects/Advent_2019/Day7/Part 2/puzzle_input_test.txt"
     with open(puzzle_input_path, 'r') as file:
         puzzle_input = file.read().strip().split(",")
         int_code = list(map(int, puzzle_input))
 
-    thruster_out_max, combo_out = amplifiers(int_code)
+
+    thruster_out_max, combo_out = PAYLOAD.amplifiers(int_code, inp_range_start=0,inp_range_end=4)
+    if loopback_mode:
+        thruster_out_max
 
     print("Max thruster output: {}".format(thruster_out_max))
     print("Amplifier Combo: {}".format(combo_out))
--- Day7/Part 2/payloads.py
+++ Day7/Part 2/payloads.py
@@ -1,16 +1,18 @@
 from itertools import permutations
-import processer
+import processor
 
 class PAYLOAD():
 
-    def amplifiers (self, code, amp_input = 0, loop_amps = True):
-        amp_inputs = permutations(range(5,10))
+    def amplifiers (self, code, inp_range_start, inp_range_end, amp_input = 0):
+        amp_inputs = permutations(range(inp_range_start,inp_range_end+1))
         thruster_out_max = 0
         for combos in amp_inputs:
             for phase in combos:
-                while loop_amps:
-                    amp_out, loop_amps = processer.processor(phase, amp_input, code)
-                    amp_input = amp_out
+                amp_out, loop_amps = processor.processor(phase, amp_input, code)
+                amp_input = amp_out
+            if amp_out > thruster_out_max:
+                thruster_out_max = amp_out
+                combo_out = combos
 
 
 
